a5bf6fa05451b5366395b9143a6e20b6e5de3ede
Vidal Graupera <vidal@vdggroup.com>
2009-05-20 11:02:00 -0700

    added rhosync auth protocol which was missing

--- rhosync.rb
+++ rhosync.rb
@@ -8,6 +8,23 @@ require 'source'
 
 require 'wikipedia'
 
+# the following two calls are to stub out the authentication used by rhosync
+
+enable :sessions
+
+post '/apps/:app_name/sources/:source_name/client_login' do
+  # return cookie with rhosync_session
+  session["rhosync_session"] = [rand(1000).to_s]
+  status 200
+end
+
+get '/apps/:app_name/sources/:source_name/clientcreate' do
+  content_type "application/json"
+  { "client" => {"client_id"=>rand(1000).to_s} }.to_json
+end
+
+# the "real work" begins here
+
 #  Parameters: {"format"=>"json", "action"=>"ask", "client_id"=>"f59a8af1-de7b-47c7-8bbb-0c421e135dc3", 
 #   "id"=>"Wikipedia", "p_size"=>"1000", "controller"=>"sources", "question"=>"life_the_universe_and_everything", 
 #   "app_id"=>"Wikipedia", "ack_token"=>"12031510061397"}
